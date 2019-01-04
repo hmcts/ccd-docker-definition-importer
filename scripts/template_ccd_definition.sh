b950ecb350252bf1ec3d326cf35343532f876989
Luigi Bitonti <luigi.bitonti@hmcts.net>
2019-01-04 15:22:37 +0000

    Use sed to process excel files

---
+++ scripts/template_ccd_definition.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+template_file_location=$1
+output_file_location=$2
+service_base_url=$3
+
+#service_base_url_placeholder='${MICROSERVICE_BASE_URL}'
+
+tmp_dir=/tmp_def
+rm -rf $tmp_dir
+mkdir $tmp_dir
+cd $tmp_dir
+
+unzip $template_file_location
+find . -type f -print0 | xargs -0 sed -i 's!\${MICROSERVICE_BASE_URL}!'"$service_base_url"'!g'
+zip -r $output_file_location .
--- scripts/upload-definition.sh
+++ scripts/upload-definition.sh
@@ -45,7 +45,7 @@ for definition in /definitions/[^~]*.xlsx # do not process excel temp files that
 do
   echo "======== PROCESSING FILE $definition ========="
 
-  /scripts/template_ccd_definition.py "$definition" /definition.xlsx "${MICROSERVICE_BASE_URL}"
+  /scripts/template_ccd_definition.sh "$definition" /definition.xlsx "${MICROSERVICE_BASE_URL}"
 
   echo "Uploading definition file"
   /scripts/import-definition.sh /definition.xlsx "${userToken}" "${serviceToken}" "${CCD_STORE_BASE_URL}"
