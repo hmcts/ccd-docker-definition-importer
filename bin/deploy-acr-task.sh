3cf451725dac6ac98b0729de49bb105d93e2cea5
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2019-10-16 09:46:27 +0100

    added acr task to build docker image same as ccd-docker-user-profile-importer (#13)

---
+++ bin/deploy-acr-task.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+GITHUB_TOKEN=$(az keyvault secret show --vault-name infra-vault-prod --name hmcts-github-apikey -o tsv --query value)
+
+az acr task create \
+    --registry hmctspublic \
+    --subscription DCD-CNP-PROD \
+    --name task-ccd-definition-importer \
+    --file acr-build-task.yaml \
+    --context https://github.com/hmcts/ccd-docker-definition-importer.git \
+    --branch master \
+    --git-access-token $GITHUB_TOKEN
