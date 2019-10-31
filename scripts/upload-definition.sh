2683ce73d1c500a5ef2c388e17b9d78f2d5e9e00
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2019-10-31 11:39:32 +0000

    using autotest credentials

--- scripts/upload-definition.sh
+++ scripts/upload-definition.sh
@@ -34,8 +34,8 @@ fi
 
 if [ "_${IMPORTER_SECRETS_MOUNT}" != "_" ]; then
   echo "Getting secrets from flex mounted volumes"
-  IMPORTER_USERNAME=$(cat ${IMPORTER_SECRETS_MOUNT}/ccd-importer-username)
-  IMPORTER_PASSWORD=$(cat ${IMPORTER_SECRETS_MOUNT}/ccd-importer-password)
+  IMPORTER_USERNAME=$(cat ${IMPORTER_SECRETS_MOUNT}/ccd-importer-autotest-email)
+  IMPORTER_PASSWORD=$(cat ${IMPORTER_SECRETS_MOUNT}/ccd-importer-autotest-password)
   CLIENT_SECRET=$(cat ${IMPORTER_SECRETS_MOUNT}/ccd-api-gateway-oauth2-client-secret)
 fi
 
