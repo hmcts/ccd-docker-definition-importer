e98d1a69104a6733e1d1f250492b87b35024fee9
anand <gkdasari@gmail.com>
2019-11-12 13:06:52 +0000

    RDM-6151-modified docker image to laetst alpine

--- bin/deploy-acr-task.sh
+++ bin/deploy-acr-task.sh
@@ -9,5 +9,5 @@ az acr task create \
     --name task-ccd-definition-importer \
     --file acr-build-task.yaml \
     --context https://github.com/hmcts/ccd-docker-definition-importer.git \
-    --branch RDM-6151 \
+    --branch master \
     --git-access-token $GITHUB_TOKEN
