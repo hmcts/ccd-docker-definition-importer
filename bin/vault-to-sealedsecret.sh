87c95589327c39ff6e430bcc7e159b3672873e41
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-11-12 22:45:31 +0000

    move to single secret

--- bin/vault-to-sealedsecret.sh
+++ bin/vault-to-sealedsecret.sh
@@ -1,34 +1,23 @@
 #!/bin/bash -x
 # script to pick up the secret from azure vault from the key provided 
-# <eg., ccd-api-gateway-oauth2-client-secret>  and create encrypted secret
+# <eg., ccd-importer-autotest-email>  and create encrypted secret
 # which will be used in Kubernetes cluster. 
 # eg, run as follows on Demo
-# vault-to-sealedsecret.sh aat /workspace/hmcts/cnp-flux-config/k8s/demo/pub-cert.pem ccd
+# vault-to-sealedsecret.sh aat /workspace/hmcts/cnp-flux-config/k8s/demo/pub-cert.pem ccd and commit importer-creds.yaml into flux
 
 env=$1
 cert=$2
 namespace=$3
-secrets=(
-    'ccd|ccd-importer-autotest-email'
-    'ccd|ccd-importer-autotest-password'
-    'ccd|ccd-api-gateway-oauth2-client-secret'
-)
 
-for i in "${secrets[@]}"; do
-    vault=$(echo "$i" | awk -F'|' '{print $1}')-$env
-    name=$(echo "$i" | awk -F'|' '{print $2}')
-    secret=$(az keyvault secret show --vault-name $vault --name $name -o tsv --query value)
-    #echo ${secret}
-
-    # kubectl delete secret $vault-$name
-
-    kubectl create secret generic $name \
-        --from-literal key=$secret \
+vault=ccd-$env
+username=$(az keyvault secret show --vault-name $vault --name ccd-importer-autotest-email -o tsv --query value)
+password=$(az keyvault secret show --vault-name $vault --name ccd-importer-autotest-password -o tsv --query value)
+idamClientSecret=$(az keyvault secret show --vault-name $vault --name ccd-api-gateway-oauth2-client-secret -o tsv --query value)
+kubectl create secret generic importer-creds \
+        --from-literal username=$username \
+        --from-literal password=$password \
+        --from-literal idam-client-secret=$idamClientSecret\
         --namespace $namespace \
-        --dry-run -o json > $name.json
+        --dry-run -o json > importer-creds.json
 
-    kubeseal --format=yaml --cert=$cert < $name.json > ../sealed-secrets/$name.yaml
-
-    # rm -f $team-$name.json
-
-done
+kubeseal --format=yaml --cert=$cert < importer-creds.json > importer-creds.yaml
