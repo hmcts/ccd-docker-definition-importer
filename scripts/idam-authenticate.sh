694de59807afb0da732b29a36f8dc5b554825465
dattipoeHMCTS <95091179+dattipoeHMCTS@users.noreply.github.com>
2022-06-28 14:38:38 +0100

    SSCS-10703 Update IDAM URLS

--- scripts/idam-authenticate.sh
+++ scripts/idam-authenticate.sh
@@ -7,6 +7,6 @@ REDIRECT_URI=$4
 CLIENT_ID=$5
 CLIENT_SECRET=$6
 
-code=$(curl ${CURL_OPTS} -k -u "${IMPORTER_USERNAME}:${IMPORTER_PASSWORD}" -XPOST "${IDAM_URI}/oauth2/authorize?redirect_uri=${REDIRECT_URI}&response_type=code&client_id=${CLIENT_ID}" -d "" | jq -r .code)
+code=$(curl ${CURL_OPTS} -k -u "${IMPORTER_USERNAME}:${IMPORTER_PASSWORD}" -XPOST "${IDAM_URI}/o/authorize?redirect_uri=${REDIRECT_URI}&response_type=code&client_id=${CLIENT_ID}" -d "" | jq -r .code)
 
-curl ${CURL_OPTS} -k -H "Content-Type: application/x-www-form-urlencoded" -u "${CLIENT_ID}:${CLIENT_SECRET}" -XPOST "${IDAM_URI}/oauth2/token?code=${code}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code" -d "" | jq -r .access_token
+curl ${CURL_OPTS} -k -H "Content-Type: application/x-www-form-urlencoded" -u "${CLIENT_ID}:${CLIENT_SECRET}" -XPOST "${IDAM_URI}/o/token?code=${code}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code" -d "" | jq -r .access_token
