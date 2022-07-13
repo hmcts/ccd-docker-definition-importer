74ee0b3d22b4b6d46d5c76c147fef3bff9acb506
Sita <sita.kandala@hmcts.net>
2022-07-13 17:53:57 +0100

    CCD-3434 update IDAM token url
    CCD-3434 update IDAM token url
    
    CCD-3434 update IDAM token url

--- scripts/idam-authenticate.sh
+++ scripts/idam-authenticate.sh
@@ -7,6 +7,4 @@ REDIRECT_URI=$4
 CLIENT_ID=$5
 CLIENT_SECRET=$6
 
-code=$(curl ${CURL_OPTS} -k -u "${IMPORTER_USERNAME}:${IMPORTER_PASSWORD}" -XPOST "${IDAM_URI}/oauth2/authorize?redirect_uri=${REDIRECT_URI}&response_type=code&client_id=${CLIENT_ID}" -d "" | jq -r .code)
-
-curl ${CURL_OPTS} -k -H "Content-Type: application/x-www-form-urlencoded" -u "${CLIENT_ID}:${CLIENT_SECRET}" -XPOST "${IDAM_URI}/oauth2/token?code=${code}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code" -d "" | jq -r .access_token
+curl -XPOST "${IDAM_URI}/o/token" -H 'Content-Type: application/x-www-form-urlencoded' -d 'grant_type=password' -d "password=${IMPORTER_PASSWORD}" -d "username=${IMPORTER_USERNAME}" -d "client_id=${CLIENT_ID}" -d "client_secret=${CLIENT_SECRET}" -d 'scope=openid profile roles'| jq -r .access_token
